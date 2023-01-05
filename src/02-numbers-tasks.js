cec79e89a6f4b400d8e0f35188e1f448e77a8bf8
DilbarAkkaya <dilbarmutavalova@gmail.com>
2023-01-06 00:36:43 +0300

    feat: implement to number

--- src/02-numbers-tasks.js
+++ src/02-numbers-tasks.js
@@ -225,8 +225,10 @@ function isPrime(n) {
  *   toNumber(42, 0) => 42
  *   toNumber(new Number(42), 0) => 42
  */
-function toNumber(/* value, def */) {
-  throw new Error('Not implemented');
+function toNumber(value, def) {
+  const numberedValue = +value;
+  if (numberedValue) return numberedValue;
+  return def;
 }
 
 module.exports = {
