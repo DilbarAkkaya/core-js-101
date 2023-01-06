469ed86eadd7c124da2505ba06d12d18dda4ac5f
DilbarAkkaya <dilbarmutavalova@gmail.com>
2023-01-06 10:11:32 +0300

    feat: implement doublearray

--- src/03-arrays-tasks.js
+++ src/03-arrays-tasks.js
@@ -52,8 +52,11 @@ function generateOdds(len) {
  *    [0, 1, 2, 3, 4, 5] => [0, 1, 2, 3, 4, 5,   0, 1, 2, 3, 4, 5]
  *    [] => []
  */
-function doubleArray(/* arr */) {
-  throw new Error('Not implemented');
+function doubleArray(arr) {
+  let res = [];
+  const clone = arr.slice(0);
+  res = [...arr, ...clone];
+  return res;
 }
 
 
