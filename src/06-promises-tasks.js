fc2a4fadad856e581335752e6b4921d1628588aa
DilbarAkkaya <dilbarmutavalova@gmail.com>
2023-02-02 18:21:40 +0300

    feat: implement processallpromises

--- src/06-promises-tasks.js
+++ src/06-promises-tasks.js
@@ -59,8 +59,8 @@ function willYouMarryMe(isPositiveAnswer) {
  *    })
  *
  */
-function processAllPromises(/* array */) {
-  throw new Error('Not implemented');
+function processAllPromises(array) {
+  return Promise.all((array)).then((el) => el);
 }
 
 /**
