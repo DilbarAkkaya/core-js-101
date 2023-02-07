c94966a58a9ac03d1d9b5f430e65aaece52e95cb
DilbarAkkaya <dilbarmutavalova@gmail.com>
2023-02-08 00:17:19 +0300

    feat: implement tonarystring

--- src/07-conditions-n-loops-tasks.js
+++ src/07-conditions-n-loops-tasks.js
@@ -345,8 +345,9 @@ function isBracketsBalanced(/* str */) {
  *    365, 4  => '11231'
  *    365, 10 => '365'
  */
-function toNaryString(/* num, n */) {
-  throw new Error('Not implemented');
+function toNaryString(num, n) {
+  const strNry = num.toString(n);
+  return strNry;
 }
 
 
