32fd2840306868b60de49aa1ac38bdf0a50e008a
DilbarAkkaya <dilbarmutavalova@gmail.com>
2023-02-12 00:00:51 +0300

    feat: implement retry

--- src/08-functions-n-closures-tasks.js
+++ src/08-functions-n-closures-tasks.js
@@ -115,8 +115,17 @@ function memoize(func) {
  * }, 2);
  * retryer() => 2
  */
-function retry(/* func, attempts */) {
-  throw new Error('Not implemented');
+function retry(func, attempts) {
+  return function retryer() {
+    for (let i = 0; i < attempts; i += 1) {
+      try {
+        return func();
+      } catch (err) {
+        // err;
+      }
+    }
+    throw new Error();
+  };
 }
 
 
