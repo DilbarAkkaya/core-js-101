b1241fdd328000fa87b4d4f219b5416df9c54567
DilbarAkkaya <dilbarmutavalova@gmail.com>
2023-02-12 01:44:16 +0300

    feat: implement getidgenerator

--- src/08-functions-n-closures-tasks.js
+++ src/08-functions-n-closures-tasks.js
@@ -194,10 +194,14 @@ function partialUsingArguments(fn, ...args1) {
  *   getId4() => 7
  *   getId10() => 11
  */
-function getIdGeneratorFunction(/* startFrom */) {
-  throw new Error('Not implemented');
+function getIdGeneratorFunction(startFrom) {
+  let i = startFrom;
+  return function getId() {
+    const integer = i;
+    i += 1;
+    return integer;
+  };
 }
-
 
 module.exports = {
   getComposition,
