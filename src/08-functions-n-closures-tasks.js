08de3c736d5b139eb602e0fe0286a8a7aad6da43
DilbarAkkaya <dilbarmutavalova@gmail.com>
2023-02-12 00:13:48 +0300

    feat: implement partialusing

--- src/08-functions-n-closures-tasks.js
+++ src/08-functions-n-closures-tasks.js
@@ -170,8 +170,10 @@ function logger(/* func, logFunc */) {
  *   partialUsingArguments(fn, 'a','b','c')('d') => 'abcd'
  *   partialUsingArguments(fn, 'a','b','c','d')() => 'abcd'
  */
-function partialUsingArguments(/* fn, ...args1 */) {
-  throw new Error('Not implemented');
+function partialUsingArguments(fn, ...args1) {
+  return function func(...args) {
+    return fn(...args1, ...args);
+  };
 }
 
 
